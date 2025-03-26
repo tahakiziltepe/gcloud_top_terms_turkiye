6d9450cfa5a3d364cba600046de2090045ab7a7a
tahakiziltepe <tahakiziltepe@hotmail.com>
2025-03-26 20:44:54 +0300

    Remove debug print statements and streamline error handling in top terms functions

--- main.py
+++ main.py
@@ -34,12 +34,10 @@ def top_terms_of_turkiye():
     try:
         top_terms_of_week = bigquery_client.get_top_terms_of_week_turkiye()
         top_terms_of_yesterday = bigquery_client.get_top_terms_of_yesterday_turkiye()
-        asyncio.run(send_message_chat.send_message("Run: get_top_terms_of_turkiye"))
         return render_template("top_terms_of_turkiye.html", 
                                top_terms_of_week=top_terms_of_week, 
                                top_terms_of_yesterday=top_terms_of_yesterday)
     except Exception as e:
-        asyncio.run(send_message_chat.send_message(f"Error: get_top_terms_of_turkiye {str(e)}"))
         return {'success': 'false', 'error': str(e)}
 
 @app.route("/send_message/top_terms_of_turkiye", methods=["GET"])
@@ -48,7 +46,6 @@ def send_message_top_terms_of_turkiye():
         top_terms_of_week = format_week_list_to_send_message(bigquery_client.get_top_terms_of_week_turkiye())
         top_terms_of_yesterday = format_day_list_to_send_message(bigquery_client.get_top_terms_of_yesterday_turkiye())
         message = "Top Terms of Week:\n" + top_terms_of_week + "\n\nTop Terms of Yesterday:\n" + top_terms_of_yesterday
-        print(message)
         asyncio.run(send_message_chat.send_message(message=message))
         return {'success': 'true', 'data': 'Message sent'}
     except Exception as e:
